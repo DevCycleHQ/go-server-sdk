21da77f5a333c14576b4b21f2a2c3fd121061947
Elliot <elliot@taplytics.com>
2021-11-24 11:50:37 -0500

    update go sdk interface

---
+++ example/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "github.com/taplytics/devcycle-go-sdk"
+    "context"
+    "log"
+)
+
+func main() {
+
+    user := devcycle.UserData{UserId: "test"}
+    auth := context.WithValue(context.Background(), devcycle.ContextAPIKey, devcycle.APIKey{
+        Key: "server-1bf0c139-6861-41e1-8d2d-ea0416045f99",
+    })
+
+    conf := devcycle.NewConfiguration()
+    client := devcycle.NewAPIClient(conf)
+    features, _ := client.DevcycleApi.GetFeatures(auth, user)
+	for key, feature := range features {
+        log.Printf("Key:%s, feature:%s", key, feature)
+    }
+
+    variables, _ := client.DevcycleApi.GetVariables(auth, user)
+    for key, variable := range variables {
+        log.Printf("Key:%s, feature:%s", key, variable)
+    }
+
+    event := devcycle.Event{
+      Type_: "customEvent",
+      Target: "somevariable.key"}
+
+    vara, _ := client.DevcycleApi.GetVariableByKey(auth, user, "elliot-asdasd", "test")
+    log.Printf("vara:%s", vara)
+
+    response, _ := client.DevcycleApi.PostEvent(auth, user, event)
+    log.Printf(response.Message)
+}
