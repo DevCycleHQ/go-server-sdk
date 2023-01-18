1c9db18d8a12846efc1425aecc209fa812d9f661
Jason Salaber <jcsalaber@hotmail.com>
2023-01-18 16:00:04 -0500

    test

--- client.go
+++ client.go
@@ -109,8 +109,6 @@ func NewDVCClient(environmentKey string, options *DVCOptions) (*DVCClient, error
 	return c, nil
 }
 
-// this is a change
-
 // selectHeaderContentType select a content type from the available list.
 func selectHeaderContentType(contentTypes []string) string {
 	if len(contentTypes) == 0 {
