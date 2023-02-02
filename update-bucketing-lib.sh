a89d76e4b1fa543fbe8c6a97eb3ed88e4049cde5
Jamie Sinn <jamie.sinn@taplytics.com>
2023-02-02 16:21:39 -0500

    Update WASM

--- update-bucketing-lib.sh
+++ update-bucketing-lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUCKETING_LIB_VERSION="1.1.25"
+BUCKETING_LIB_VERSION="1.1.28"
 WAT_DOWNLOAD=0
 rm bucketing-lib.release.wasm
 wget "https://unpkg.com/@devcycle/bucketing-assembly-script@$BUCKETING_LIB_VERSION/build/bucketing-lib.release.wasm"
