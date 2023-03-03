2a22fe7a1850d230684c3b4ddc525d36af92a553
Adam Wootton <adam@taplytics.com>
2023-03-03 17:11:38 -0500

    handle nil result pointer

--- localbucketing.go
+++ localbucketing.go
@@ -422,6 +422,12 @@ func (d *DevCycleLocalBucketing) VariableForUser(user string, key string) (ret V
 	if err != nil {
 		return
 	}
+	
+	if varPtr == nil {
+		ret = Variable{}
+		return
+	}
+
 	if errorMessage != "" {
 		err = fmt.Errorf(errorMessage)
 		return
