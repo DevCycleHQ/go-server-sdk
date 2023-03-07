b535fd8cf640281090fdd82b892cecd63274358a
Rob O'Dwyer <126593209+rob-odwyer@users.noreply.github.com>
2023-03-06 16:08:41 -0800

    DVC-6496: add benchmark and sanitized config (#95)
    add BenchmarkDVCClient_Variable benchmark and sanitized config

--- testing_helpers.go
+++ testing_helpers.go
@@ -1,14 +1,29 @@
 package devcycle
 
 import (
-	"github.com/jarcoal/httpmock"
+	_ "embed"
 	"net/http"
+	"strings"
+
+	"github.com/jarcoal/httpmock"
 )
 
 var (
-	test_config         = `{"project":{"settings":{"edgeDB":{"enabled":false},"optIn":{"enabled":true,"title":"Beta Feature Access","description":"Get early access to new features below","imageURL":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR68cgQT_BTgnhWTdfjUXSN8zM9Vpxgq82dhw&usqp=CAU","colors":{"primary":"#0042f9","secondary":"#facc15"}}},"a0_organization":"org_NszUFyWBFy7cr95J","_id":"6216420c2ea68943c8833c09","key":"default"},"environment":{"_id":"6216420c2ea68943c8833c0b","key":"development"},"features":[{"_id":"6216422850294da359385e8b","key":"test","type":"release","variations":[{"variables":[{"_var":"6216422850294da359385e8d","value":true}],"name":"Variation On","key":"variation-on","_id":"6216422850294da359385e8f"},{"variables":[{"_var":"6216422850294da359385e8d","value":false}],"name":"Variation Off","key":"variation-off","_id":"6216422850294da359385e90"}],"configuration":{"_id":"621642332ea68943c8833c4a","targets":[{"distribution":[{"percentage":0.5,"_variation":"6216422850294da359385e8f"},{"percentage":0.5,"_variation":"6216422850294da359385e90"}],"_audience":{"_id":"621642332ea68943c8833c4b","filters":{"operator":"and","filters":[{"values":[],"type":"all","filters":[]}]}},"_id":"621642332ea68943c8833c4d"}],"forcedUsers":{}}}],"variables":[{"_id":"6216422850294da359385e8d","key":"test","type":"Boolean"}],"variableHashes":{"test":2447239932}}`
 	test_environmentKey = "dvc_server_token_hash"
+
+	//go:embed testdata/fixture_small_config.json
+	test_config string
+
+	//go:embed testdata/fixture_large_config.json
+	test_large_config          string
+	test_large_config_variable = "v-key-25"
 )
+
+func init() {
+	// Remove newlines in configs
+	test_config = strings.ReplaceAll(test_config, "\n", "")
+	test_large_config = strings.ReplaceAll(test_large_config, "\n", "")
+}
 
 func httpBucketingAPIMock() {
 	httpmock.RegisterResponder("POST", "https://bucketing-api.devcycle.com/v1/variables/test",
@@ -27,10 +42,13 @@ func httpEventsApiMock() {
 }
 
 func httpConfigMock(respcode int) {
-	httpmock.RegisterResponder("GET", "https://config-cdn.devcycle.com/config/v1/server/dvc_server_token_hash.json",
-		func(req *http.Request) (*http.Response, error) {
+	httpCustomConfigMock(test_environmentKey, respcode, test_config)
+}
 
-			resp := httpmock.NewStringResponse(respcode, test_config)
+func httpCustomConfigMock(sdkKey string, respcode int, config string) {
+	httpmock.RegisterResponder("GET", "https://config-cdn.devcycle.com/config/v1/server/"+sdkKey+".json",
+		func(req *http.Request) (*http.Response, error) {
+			resp := httpmock.NewStringResponse(respcode, config)
 			resp.Header.Set("Etag", "TESTING")
 			return resp, nil
 		},
