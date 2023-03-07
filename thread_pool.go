e5c331414003837a7a7436ac5be24e29ba78f9c1
Adam Wootton <adam@taplytics.com>
2023-03-07 15:20:47 -0500

    move lock to wait until ready

--- thread_pool.go
+++ thread_pool.go
@@ -50,6 +50,7 @@ func (w *LocalBucketingWorker) Initialize(sdkKey string, options *DVCOptions) (e
 
 func (w *LocalBucketingWorker) Process(payload interface{}) interface{} {
 	var workerPayload = payload.(*VariableForUserPayload)
+	defer w.workerMutex.Unlock()
 	if workerPayload.Type_ == VariableForUser {
 		var variableForUserPayload = payload.(*VariableForUserPayload)
 
@@ -68,8 +69,6 @@ func (w *LocalBucketingWorker) Process(payload interface{}) interface{} {
 }
 
 func (w *LocalBucketingWorker) variableForUser(user *[]byte, key *string, variableType VariableTypeCode) (Variable, error) {
-	w.workerMutex.Lock()
-	defer w.workerMutex.Unlock()
 	return w.localBucketing.VariableForUser(*user, *key, variableType)
 }
 
@@ -79,6 +78,8 @@ func (w *LocalBucketingWorker) StoreConfig(configData []byte) error {
 	return w.localBucketing.StoreConfig(configData)
 }
 
-func (w *LocalBucketingWorker) BlockUntilReady() {}
-func (w *LocalBucketingWorker) Interrupt()       {}
-func (w *LocalBucketingWorker) Terminate()       {}
+func (w *LocalBucketingWorker) BlockUntilReady() {
+	w.workerMutex.Lock()
+}
+func (w *LocalBucketingWorker) Interrupt() {}
+func (w *LocalBucketingWorker) Terminate() {}
