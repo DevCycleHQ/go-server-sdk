874a045f0372a0a53e577f3a9405ab053d7ddb82
Jamie Sinn <jamie.sinn@taplytics.com>
2023-03-15 15:22:29 -0400

    Close body after making the request successfuly for a config.

--- configmanager.go
+++ configmanager.go
@@ -71,6 +71,7 @@ func (e *EnvironmentConfigManager) fetchConfig(retrying bool) error {
 	if err != nil {
 		return err
 	}
+	defer resp.Body.Close()
 	switch statusCode := resp.StatusCode; {
 	case statusCode == http.StatusOK:
 		if err = e.setConfig(resp); err != nil {
@@ -105,11 +106,11 @@ func (e *EnvironmentConfigManager) fetchConfig(retrying bool) error {
 }
 
 func (e *EnvironmentConfigManager) setConfig(response *http.Response) error {
+
 	raw, err := io.ReadAll(response.Body)
 	if err != nil {
 		return err
 	}
-
 	// Check
 	valid := json.Valid(raw)
 	if !valid {
