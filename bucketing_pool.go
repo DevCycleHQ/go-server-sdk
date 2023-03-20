5b10a7a99f3c60a4896880d7d3a4623e4ae3c7fc
Adam Wootton <adam@taplytics.com>
2023-03-20 14:24:23 -0400

    swap two pools

--- bucketing_pool.go
+++ bucketing_pool.go
@@ -6,11 +6,15 @@ import (
 	"github.com/devcyclehq/go-server-sdk/v2/proto"
 	pool "github.com/jolestar/go-commons-pool/v2"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 type BucketingPool struct {
-	pool             *pool.ObjectPool
+	pool1            *pool.ObjectPool
+	pool2            *pool.ObjectPool
+	currentPool      atomic.Pointer[pool.ObjectPool]
+	poolSwapMutex    sync.Mutex
 	ctx              context.Context
 	factory          *BucketingPoolFactory
 	configData       *[]byte
@@ -36,12 +40,20 @@ func NewBucketingPool(ctx context.Context, wasmMain *WASMMain, sdkKey string, op
 	bucketingPool.factory = MakeBucketingPoolFactory(wasmMain, sdkKey, options, bucketingPool)
 
 	bucketingPool.isFlushingMutex = sync.Mutex{}
+	bucketingPool.poolSwapMutex = sync.Mutex{}
 
-	bucketingPool.pool = pool.NewObjectPool(ctx, bucketingPool.factory, config)
+	bucketingPool.pool1 = pool.NewObjectPool(ctx, bucketingPool.factory, config)
+	bucketingPool.pool2 = pool.NewObjectPool(ctx, bucketingPool.factory, config)
 	bucketingPool.eventFlushChan = make(chan *PayloadsAndChannel)
 
+	bucketingPool.currentPool.Store(bucketingPool.pool1)
+
 	for i := 0; i < options.MaxWasmWorkers; i++ {
-		err := bucketingPool.pool.AddObject(ctx)
+		err := bucketingPool.pool1.AddObject(ctx)
+		if err != nil {
+			return nil, err
+		}
+		err = bucketingPool.pool2.AddObject(ctx)
 		if err != nil {
 			return nil, err
 		}
@@ -50,12 +62,13 @@ func NewBucketingPool(ctx context.Context, wasmMain *WASMMain, sdkKey string, op
 }
 
 func (p *BucketingPool) VariableForUser(paramsBuffer []byte) (*proto.SDKVariable_PB, error) {
-	bucketing, err := p.pool.BorrowObject(p.ctx)
+	currentPool := p.currentPool.Load()
+	bucketing, err := currentPool.BorrowObject(p.ctx)
 	if err != nil {
 		return nil, err
 	}
 	defer func() {
-		err = p.pool.ReturnObject(p.ctx, bucketing)
+		err = currentPool.ReturnObject(p.ctx, bucketing)
 		if err != nil {
 			// TODO do we need to panic here?
 			panic(err)
@@ -72,15 +85,15 @@ func (p *BucketingPool) VariableForUser(paramsBuffer []byte) (*proto.SDKVariable
 	return variablePB, nil
 }
 
-func (p *BucketingPool) pokeOne() error {
-	bucketing, err := p.pool.BorrowObject(p.ctx)
+func (p *BucketingPool) pokeOne(currentPool *pool.ObjectPool) error {
+	bucketing, err := currentPool.BorrowObject(p.ctx)
 	if err != nil {
 		return err
 	}
 
 	defer func() {
 		go func() {
-			err = p.pool.ReturnObject(p.ctx, bucketing)
+			err = currentPool.ReturnObject(p.ctx, bucketing)
 			if err != nil {
 				// TODO do we need to panic here?
 				panic(err)
@@ -95,9 +108,10 @@ func (p *BucketingPool) pokeOne() error {
 // assuming that any objects that weren't borrowed during this process were busy and thus passivate will be called anyway
 func (p *BucketingPool) PokeAll() error {
 	printf("Poking all workers")
+	currentPool := p.currentPool.Load()
 
-	for i := 0; i < p.pool.Config.MaxTotal; i++ {
-		err := p.pokeOne()
+	for i := 0; i < currentPool.Config.MaxTotal; i++ {
+		err := p.pokeOne(currentPool)
 
 		if err != nil {
 			return err
@@ -107,56 +121,84 @@ func (p *BucketingPool) PokeAll() error {
 	return nil
 }
 
+// ProcessAll will func the "process" handler function on every object in the pool. It will block until the operation
+// has completed for every object, or there was an error. It naively grabs the longest idle object from the pool each
+// time and checks if it has seen it before. If it has, it will immediately return it and try again.
 func (p *BucketingPool) ProcessAll(
 	operationName string,
 	process func(object *BucketingPoolObject) error,
 	timeout time.Duration,
 ) (err error) {
+	p.poolSwapMutex.Lock()
+	defer p.poolSwapMutex.Unlock()
+
+	currentPool := p.currentPool.Load()
+
+	inactivePool := p.pool1
+	if currentPool == p.pool1 {
+		inactivePool = p.pool2
+	}
+
 	idMap := make(map[int32]bool)
-	//start := time.Now()
+	start := time.Now()
 	ctx, cancel := context.WithTimeout(p.ctx, timeout)
 
 	defer cancel()
 
-	//debugf("Borrowing all objects from pool for %s (max %d)", operationName, p.pool.Config.MaxTotal)
-	for len(idMap) < p.pool.Config.MaxTotal && ctx.Err() == nil {
-		var curObj *BucketingPoolObject
-		for curObj == nil {
-			borrowed, err := p.pool.BorrowObject(ctx)
-			curObj = borrowed.(*BucketingPoolObject)
+	wrongObjectCount := 0
+
+	processAll := func(processPool *pool.ObjectPool) error {
+		for len(idMap) < processPool.Config.MaxTotal && ctx.Err() == nil {
+			var curObj *BucketingPoolObject
+			for curObj == nil {
+				borrowed, err := processPool.BorrowObject(ctx)
+				curObj = borrowed.(*BucketingPoolObject)
+				if err != nil {
+					_ = processPool.ReturnObject(p.ctx, curObj)
+					if ctx.Err() != nil {
+						return fmt.Errorf("timed out after %s while processing %s on all workers", timeout, operationName)
+					}
+					return err
+				}
+
+				if idMap[curObj.id] {
+					_ = processPool.ReturnObject(p.ctx, curObj)
+					wrongObjectCount += 1
+					curObj = nil
+				}
+			}
+
+			idMap[curObj.id] = true
+
+			err := process(curObj)
+			_ = processPool.ReturnObject(p.ctx, curObj)
+
 			if err != nil {
-				_ = p.pool.ReturnObject(ctx, curObj)
 				return err
 			}
-
-			if idMap[curObj.id] {
-				_ = p.pool.ReturnObject(p.ctx, curObj)
-				curObj = nil
-			}
 		}
 
-		idMap[curObj.id] = true
-
-		err := process(curObj)
-		_ = p.pool.ReturnObject(ctx, curObj)
-
-		if err != nil {
-			return err
-		}
+		return nil
 	}
 
-	if ctx.Err() != nil {
-		return fmt.Errorf("timed out after %s while processing %s on all workers", timeout, operationName)
+	err = processAll(inactivePool)
+	if err != nil {
+		return err
 	}
 
-	//debugf("Borrowed all objects from pool for %s in %s", operationName, time.Since(start))
+	p.currentPool.Swap(inactivePool)
+	err = processAll(currentPool)
+
+	//debugf("Borrowing all objects from pool for %s (max %d)", operationName, p.pool.Config.MaxTotal)
+
+	debugf("Borrowed all objects from pool for %s in %s with %d repeated objects", operationName, time.Since(start), wrongObjectCount)
 
 	return nil
 }
 
 func (p *BucketingPool) SetConfig(config []byte) error {
 	p.configData = &config
-	debugf("Setting config on all workers (max %d)", p.pool.Config.MaxTotal)
+	debugf("Setting config on all workers")
 	return p.ProcessAll("SetConfig", func(object *BucketingPoolObject) error {
 		return object.StoreConfig(&config)
 	}, 5*time.Second)
@@ -164,7 +206,7 @@ func (p *BucketingPool) SetConfig(config []byte) error {
 
 func (p *BucketingPool) SetClientCustomData(customData []byte) error {
 	p.clientCustomData = &customData
-	debugf("Setting client custom data on all workers (max %d)", p.pool.Config.MaxTotal)
+	debugf("Setting client custom data on all workers")
 	return p.ProcessAll("SetClientCustomData", func(object *BucketingPoolObject) error {
 		return object.SetClientCustomData(&customData)
 	}, 5*time.Second)
