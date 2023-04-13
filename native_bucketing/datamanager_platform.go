446abe0e0730e05211a07750ffa413c0fc186127
Jamie Sinn <jamie.sinn@taplytics.com>
2023-04-13 15:33:26 -0400

    Also fix platformData to allow overriding specific settings per version

--- native_bucketing/datamanager_platform.go
+++ native_bucketing/datamanager_platform.go
@@ -2,13 +2,9 @@ package native_bucketing
 
 import (
 	"fmt"
-	"runtime"
 )
 
 var platformDataMap = map[string]PlatformData{}
-var emptyPlatformData = PlatformData{}
-
-var VERSION = "0.0.0"
 
 func GetPlatformData(token string) (data *PlatformData, err error) {
 	if data, ok := platformDataMap[token]; ok {
@@ -17,14 +13,8 @@ func GetPlatformData(token string) (data *PlatformData, err error) {
 	return nil, fmt.Errorf("no platform data found for token %s", token)
 }
 
-func SetPlatformData(token string) {
-	data := PlatformData{
-		Platform:        "Go",
-		PlatformVersion: runtime.Version(),
-		SdkType:         "server",
-		SdkVersion:      VERSION,
-	}
-	platformDataMap[token] = data
+func SetPlatformData(sdkKey string, data PlatformData) {
+	platformDataMap[sdkKey] = data
 }
 
 var clientCustomData = map[string]map[string]interface{}{}
