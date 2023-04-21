6c2845e86556e6bb6cf351c2fcd66e40d77e99c5
Chris Hoefgen <58341173+chris-hoefgen@users.noreply.github.com>
2023-04-21 14:20:14 -0700

    Adding segmentation unit tests (#165)
    * Adding tests to improve coverage
    
    * tests around version checking
    
    * Added some basic unit testing for audience filtering
    
    * added a couple more tests
    
    * cleanup

--- native_bucketing/segmentation_test.go
+++ native_bucketing/segmentation_test.go
@@ -1,6 +1,7 @@
 package native_bucketing
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -84,55 +85,121 @@ func TestEvaluateOperator_InvalidComparator(t *testing.T) {
 }
 
 func TestEvaluateOperator_AudienceFilterMatch(t *testing.T) {
-	userFilters := MixedFilters{
-		&UserFilter{
-			filter: filter{
-				Type:       "user",
-				SubType:    "email",
-				Comparator: "=",
-			},
-			Values: []interface{}{"dexter@smells.nice", "brooks@big.lunch"},
-		},
-		&UserFilter{
-			filter: filter{
-				Type:       "user",
-				SubType:    "country",
-				Comparator: "=",
-			},
-			Values: []interface{}{"Canada"}},
-		&UserFilter{
-			filter: filter{
-				Type:       "user",
-				SubType:    "appVersion",
-				Comparator: ">",
-			},
-			Values: []interface{}{"1.0.0"}},
-	}
-	_ = AudienceOperator{
-		Operator: "and",
-		Filters:  userFilters,
-	}
-	audienceMatchEqual := AudienceMatchFilter{
+	countryFilter := &UserFilter{
 		filter: filter{
-			Type:       "audienceMatch",
+			Type:       "user",
+			SubType:    "country",
 			Comparator: "=",
+			Operator:   OperatorAnd,
 		},
-		Audiences: []string{"test"},
+		Values: []interface{}{"Canada"},
 	}
-	_ = AudienceMatchFilter{
+	require.NoError(t, countryFilter.Initialize())
+	emailFilter := &UserFilter{
 		filter: filter{
-			Type:       "audienceMatch",
-			Comparator: "!=",
+			Type:       "user",
+			SubType:    "email",
+			Comparator: "=",
+			Operator:   OperatorAnd,
 		},
-		Audiences: []string{"test"},
+		Values: []interface{}{
+			"dexter@smells.nice",
+			"brooks@big.lunch",
+		},
 	}
-	var filters = []BaseFilter{audienceMatchEqual}
-
-	_ = OperatorFilter{
-		Operator: &AudienceOperator{
-			Operator: "and",
-			Filters:  filters,
+	require.NoError(t, emailFilter.Initialize())
+	versionFilter := &UserFilter{
+		filter: filter{
+			Type:       "user",
+			SubType:    "appVersion",
+			Comparator: ">",
+			Operator:   OperatorAnd,
 		},
+		Values: []interface{}{
+			"1.0.0",
+		},
+	}
+	require.NoError(t, versionFilter.Initialize())
+
+	audience := Audience{
+		NoIdAudience: NoIdAudience{
+			Filters: &AudienceOperator{
+				Operator: OperatorAnd,
+				Filters: []BaseFilter{
+					countryFilter,
+					emailFilter,
+					versionFilter,
+				},
+			},
+		},
+		Id: "test",
+	}
+
+	testCases := []struct {
+		name      string
+		filters   []BaseFilter
+		audiences map[string]NoIdAudience
+		expected  bool
+	}{
+		{
+			name: "audienceMatchFilter - in the audience",
+			filters: []BaseFilter{AudienceMatchFilter{
+				filter: filter{
+					Type:       "audienceMatch",
+					Comparator: "=",
+					Operator:   OperatorAnd,
+				},
+				Audiences: []string{"test"},
+			}},
+			audiences: map[string]NoIdAudience{"test": audience.NoIdAudience},
+			expected:  true,
+		},
+		{
+			name: "audienceMatchFilter - not in the audience",
+			filters: []BaseFilter{AudienceMatchFilter{
+				filter: filter{
+					Type:       "audienceMatch",
+					Comparator: "!=",
+					Operator:   OperatorAnd,
+				},
+				Audiences: []string{"test"},
+			}},
+			audiences: map[string]NoIdAudience{"test": audience.NoIdAudience},
+			expected:  false,
+		},
+		{
+			name: "audienceMatchFilter - audience ID not in list",
+			filters: []BaseFilter{AudienceMatchFilter{
+				filter: filter{
+					Type:       "audienceMatch",
+					Comparator: "==",
+					Operator:   OperatorAnd,
+				},
+				Audiences: []string{"test"},
+			}},
+			audiences: map[string]NoIdAudience{},
+			expected:  false,
+		},
+		{
+			name: "audienceMatchFilter - audience ID not in list",
+			filters: []BaseFilter{AudienceMatchFilter{
+				filter: filter{
+					Type:       "audienceMatch",
+					Comparator: "==",
+					Operator:   OperatorAnd,
+				},
+				Audiences: []string{"someOtherAudienceID"},
+			}},
+			audiences: map[string]NoIdAudience{"test": audience.NoIdAudience},
+			expected:  false,
+		},
+	}
+
+	for _, tc := range testCases {
+		result := _evaluateOperator(AudienceOperator{Operator: "and", Filters: tc.filters}, tc.audiences, brooks, nil)
+		if result != tc.expected {
+			t.Errorf("%v - Expected %t, got %t", tc.name, tc.expected, result)
+		}
 	}
 }
 
@@ -437,5 +504,172 @@ func TestEvaluateOperator_AndPrivateCustomDataMultiValue(t *testing.T) {
 	result := _evaluateOperator(AudienceOperator{Operator: "and", Filters: MixedFilters{customDataFilter}}, nil, brooks, nil)
 	if result {
 		t.Error("Expected false, got true")
+	}
+}
+
+func Test_checkVersionValue(t *testing.T) {
+	testCases := []struct {
+		filterVersion string
+		version       string
+		operator      string
+		expected      bool
+	}{
+		{
+			filterVersion: "1.2.3",
+			version:       "1.2.3",
+			operator:      "==",
+			expected:      true,
+		},
+		{
+			filterVersion: "1.2.3",
+			version:       "2.3.4",
+			operator:      "==",
+			expected:      false,
+		},
+		{
+			filterVersion: "1.2.3",
+			version:       "2.3.4",
+			operator:      ">",
+			expected:      true,
+		},
+		{
+			filterVersion: "2.3.4",
+			version:       "1.2.3",
+			operator:      ">",
+			expected:      false,
+		},
+		{
+			filterVersion: "2.3.4",
+			version:       "1.2.3",
+			operator:      "<",
+			expected:      true,
+		},
+		{
+			filterVersion: "1.2.3",
+			version:       "2.3.4",
+			operator:      "<",
+			expected:      false,
+		},
+	}
+
+	for _, tc := range testCases {
+		got := checkVersionValue(tc.filterVersion, tc.version, tc.operator)
+		if got != tc.expected {
+			t.Errorf("checkVersionValue(%s, %s, %s) = %v; want %v", tc.filterVersion, tc.version, tc.operator, got, tc.expected)
+		}
+	}
+}
+
+func Test_ConvertToSemanticVersion(t *testing.T) {
+	testCases := []struct {
+		input  string
+		output string
+	}{
+		{
+			input:  "1.2.3",
+			output: "1.2.3",
+		},
+		{
+			input:  "1.2",
+			output: "1.2.0",
+		},
+		{
+			input:  "1",
+			output: "1.0.0",
+		},
+		{
+			input:  "1..3",
+			output: "1.0.3",
+		},
+		{
+			input:  "1.2.3.4",
+			output: "1.2.3.4",
+		},
+	}
+
+	for _, tc := range testCases {
+		got := convertToSemanticVersion(tc.input)
+		if got != tc.output {
+			t.Errorf("convertToSemanticVersion(%s) = %s; want %s", tc.input, got, tc.output)
+		}
+	}
+}
+
+func Test_CheckNumberFilter(t *testing.T) {
+	type NumberTestCase struct {
+		num        float64
+		filterNums []float64
+		operator   string
+		want       bool
+	}
+
+	testCases := []NumberTestCase{
+		{num: math.NaN(), filterNums: []float64{}, operator: "", want: false},
+		{num: math.NaN(), filterNums: []float64{}, operator: "exist", want: false},
+		{num: math.NaN(), filterNums: []float64{}, operator: "!exist", want: true},
+
+		{num: 10, filterNums: []float64{10}, operator: "=", want: true},
+		{num: 10, filterNums: []float64{10, 20}, operator: "=", want: true},
+		{num: 10, filterNums: []float64{}, operator: "=", want: false},
+		{num: 10, filterNums: []float64{math.NaN()}, operator: "=", want: false},
+
+		{num: 10, filterNums: []float64{5, 10, 15}, operator: ">", want: true},
+		{num: 10, filterNums: []float64{}, operator: ">", want: false},
+		{num: 10, filterNums: []float64{10}, operator: ">", want: false},
+		{num: 10, filterNums: []float64{15}, operator: ">", want: false},
+
+		{num: 10, filterNums: []float64{5, 10, 15}, operator: ">=", want: true},
+		{num: 10, filterNums: []float64{}, operator: ">=", want: false},
+		{num: 10, filterNums: []float64{10}, operator: ">=", want: true},
+		{num: 10, filterNums: []float64{15}, operator: ">=", want: false},
+
+		{num: 10, filterNums: []float64{5, 10, 15}, operator: "<", want: true},
+		{num: 10, filterNums: []float64{}, operator: "<", want: false},
+		{num: 10, filterNums: []float64{10}, operator: "<", want: false},
+		{num: 10, filterNums: []float64{15}, operator: "<", want: true},
+
+		{num: 10, filterNums: []float64{5, 10, 15}, operator: "<=", want: true},
+		{num: 10, filterNums: []float64{}, operator: "<=", want: false},
+		{num: 10, filterNums: []float64{10}, operator: "<=", want: true},
+		{num: 10, filterNums: []float64{15}, operator: "<=", want: true},
+
+		{num: 10, filterNums: []float64{5, 15}, operator: "!=", want: true},
+		{num: 10, filterNums: []float64{}, operator: "!=", want: false},
+		{num: 10, filterNums: []float64{math.NaN()}, operator: "!=", want: false},
+
+		{num: 10, filterNums: []float64{}, operator: "fakeop", want: false},
+		{num: 10, filterNums: []float64{10}, operator: "fakeop", want: false},
+	}
+
+	for _, tc := range testCases {
+		got := _checkNumberFilter(tc.num, tc.filterNums, tc.operator)
+		if got != tc.want {
+			t.Errorf("_checkNumberFilter(%v, %v, %s) = %v; want %v", tc.num, tc.filterNums, tc.operator, got, tc.want)
+		}
+	}
+}
+
+func TestCheckValueExists(t *testing.T) {
+	type ValueTestCase struct {
+		value interface{}
+		want  bool
+	}
+
+	testCases := []ValueTestCase{
+		{value: "test", want: true},
+		{value: 123, want: true},
+		{value: true, want: true},
+		{value: 1.23, want: true},
+		{value: nil, want: false},
+		{value: "", want: false},
+		{value: math.NaN(), want: false},
+		{value: struct{}{}, want: false},
+	}
+
+	for _, tc := range testCases {
+		got := checkValueExists(tc.value)
+		if got != tc.want {
+			t.Errorf("checkValueExists(%v) = %v; want %v", tc.value, got, tc.want)
+		}
 	}
 }
