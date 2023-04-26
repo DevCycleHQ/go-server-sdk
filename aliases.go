5074737e072dc9ff27eaed8a28e8ed21b9110f07
Jamie Sinn <jamie.sinn@taplytics.com>
2023-04-26 17:16:42 -0400

    Fix aliases

--- aliases.go
+++ aliases.go
@@ -4,25 +4,25 @@ import (
 	"github.com/devcyclehq/go-server-sdk/v2/api"
 )
 
-type ErrorResponse api.ErrorResponse
-type BucketedUserConfig api.BucketedUserConfig
-type Environment api.Environment
-type BaseVariable api.BaseVariable
-type Variable api.Variable
-type ReadOnlyVariable api.ReadOnlyVariable
-type DVCUser api.DVCUser
-type DVCPopulatedUser api.DVCPopulatedUser
-type UserFeatureData api.UserFeatureData
-type UserDataAndEventsBody api.UserDataAndEventsBody
-type Project api.Project
-type ProjectSettings api.ProjectSettings
-type EdgeDBSettings api.EdgeDBSettings
-type OptInSettings api.OptInSettings
-type OptInColors api.OptInColors
-type PlatformData api.PlatformData
-type FeatureVariation api.FeatureVariation
-type DVCEvent api.DVCEvent
-type UserEventsBatchRecord api.UserEventsBatchRecord
-type FlushPayload api.FlushPayload
-type BatchEventsBody api.BatchEventsBody
-type Feature api.Feature
+type ErrorResponse = api.ErrorResponse
+type BucketedUserConfig = api.BucketedUserConfig
+type Environment = api.Environment
+type BaseVariable = api.BaseVariable
+type Variable = api.Variable
+type ReadOnlyVariable = api.ReadOnlyVariable
+type DVCUser = api.DVCUser
+type DVCPopulatedUser = api.DVCPopulatedUser
+type UserFeatureData = api.UserFeatureData
+type UserDataAndEventsBody = api.UserDataAndEventsBody
+type Project = api.Project
+type ProjectSettings = api.ProjectSettings
+type EdgeDBSettings = api.EdgeDBSettings
+type OptInSettings = api.OptInSettings
+type OptInColors = api.OptInColors
+type PlatformData = api.PlatformData
+type FeatureVariation = api.FeatureVariation
+type DVCEvent = api.DVCEvent
+type UserEventsBatchRecord = api.UserEventsBatchRecord
+type FlushPayload = api.FlushPayload
+type BatchEventsBody = api.BatchEventsBody
+type Feature = api.Feature
