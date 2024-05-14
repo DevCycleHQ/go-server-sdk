4ffb38c0875316788c1bb44e3b3c826977fd6ea2
Jamie Sinn <jamie.sinn@taplytics.com>
2024-05-14 15:34:28 -0400

    Rename/move model types

--- api/model_event.go
+++ api/model_event.go
@@ -13,6 +13,25 @@ const (
 	EventType_CustomEvent          = "customEvent"
 )
 
+type ClientEvent struct {
+	EventType ClientEventType `json:"eventType"`
+	EventData interface{}     `json:"eventData"`
+	Status    string          `json:"status"`
+	Error     error           `json:"error"`
+}
+
+type ClientEventType string
+
+const (
+	ClientEventType_Initialized                ClientEventType = "initialized"
+	ClientEventType_Error                      ClientEventType = "error"
+	ClientEventType_ConfigUpdated              ClientEventType = "configUpdated"
+	ClientEventType_RealtimeUpdates            ClientEventType = "realtimeUpdates"
+	ClientEventType_InternalSSEFailure         ClientEventType = "internalSSEFailure"
+	ClientEventType_InternalNewConfigAvailable ClientEventType = "internalNewConfigAvailable"
+	ClientEventType_InternalSSEConnected       ClientEventType = "internalSSEConnected"
+)
+
 type Event struct {
 	Type_       string                 `json:"type"`
 	Target      string                 `json:"target,omitempty"`
