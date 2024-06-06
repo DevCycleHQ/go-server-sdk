de58b7a9779b4b03008b5db206939270252cdb67
Jamie Sinn <jamie.sinn@taplytics.com>
2024-06-06 15:28:31 -0400

    fix userid

--- event_manager.go
+++ event_manager.go
@@ -126,7 +126,7 @@ func (e *EventManager) QueueSDKConfigEvent(req http.Request, resp http.Response)
 
 	event := api.Event{
 		Type_:  api.EventType_SDKConfig,
-		UserId: uuid,
+		UserId: user.UserId,
 		Target: fmt.Sprintf("%s://%s%s", req.URL.Scheme, req.URL.Host, req.URL.Path),
 		Value:  -1,
 		MetaData: map[string]interface{}{
