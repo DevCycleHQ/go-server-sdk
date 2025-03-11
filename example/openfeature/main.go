af912affa70b9ddd38deb544feed5ec4b0bebe27
Jamie Sinn <jamie@devcycle.com>
2025-03-11 15:38:37 -0400

    feat: Openfeature Provider Update (#280)
    * wip track provider and init handling
    
    * Support provider status and tracking events
    - Rework flattened provider handler
    
    * fix example app
    
    * fix example app init handling
    
    * fix close handling
    
    * increase wait time

--- example/openfeature/main.go
+++ example/openfeature/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"context"
+	"github.com/devcyclehq/go-server-sdk/v2/api"
 	"log"
 	"os"
 	"time"
 
 	devcycle "github.com/devcyclehq/go-server-sdk/v2"
-	"github.com/open-feature/go-sdk/pkg/openfeature"
+	"github.com/open-feature/go-sdk/openfeature"
 )
 
 func main() {
@@ -15,7 +16,7 @@ func main() {
 	if sdkKey == "" {
 		log.Fatal("DEVCYCLE_SERVER_SDK_KEY env var not set: set it to your SDK key")
 	}
-
+	inithandler := make(chan api.ClientEvent, 10)
 	dvcOptions := devcycle.Options{
 		EnableEdgeDB:                 false,
 		EnableCloudBucketing:         false,
@@ -24,10 +25,22 @@ func main() {
 		RequestTimeout:               time.Second * 10,
 		DisableAutomaticEventLogging: false,
 		DisableCustomEventLogging:    false,
+		ClientEventHandler:           inithandler,
 	}
-	dvcClient, _ := devcycle.NewClient(sdkKey, &dvcOptions)
+	provider, err := devcycle.NewDevCycleProvider(sdkKey, &dvcOptions)
+	if err != nil {
+		log.Fatalf("Failed to create DevCycle client: %v", err)
+	}
 
-	if err := openfeature.SetProvider(dvcClient.OpenFeatureProvider()); err != nil {
+	// Wait for the client to be initialized
+	for {
+		time.Sleep(time.Millisecond * 100)
+		event := <-inithandler
+		if event.EventType == api.ClientEventType_Initialized {
+			break
+		}
+	}
+	if err = openfeature.SetProvider(provider); err != nil {
 		log.Fatalf("Failed to set DevCycle provider: %v", err)
 	}
 	client := openfeature.NewClient("devcycle")
